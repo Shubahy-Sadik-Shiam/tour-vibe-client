855d116a28801b93da2061138cb3e51903693eb3
ShorgoShiam <shorgoshiam@gmail.com>
2025-02-19 18:15:00 +0600

    implement sort by price on Trips page

--- src/pages/AllTours.jsx
+++ src/pages/AllTours.jsx
@@ -1,14 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../hooks/useAxiosPublic";
 import TourCard from "../components/TourCard";
+import { useState } from "react";
 
 const AllTours = () => {
   const axiosPublic = useAxiosPublic();
+  const [sortOrder, setSortOrder] = useState("asc");
+
   const { data: allTours = [], isLoading } = useQuery({
-    queryKey: ["allTours"],
+    queryKey: ["allTours", sortOrder],
     queryFn: async () => {
       const res = await axiosPublic.get("/packages");
-      return res.data;
+      let tours = res.data;
+
+      tours.sort((a, b) => 
+        sortOrder === "asc" ? a.price - b.price : b.price - a.price
+      );
+      return tours;
     },
   });
   if (isLoading) {
@@ -27,6 +35,14 @@ const AllTours = () => {
         From Dream Destinations to Hidden Gems: <br />
         Discover Our Exclusive Tour Packages
       </h2>
+      <div className="flex justify-end my-10">
+        <button
+          onClick={() => setSortOrder(sortOrder === "asc" ? "desc" : "asc")}
+          className="px-4 py-2 bg-teal-500 text-white font-semibold rounded-lg shadow-md hover:bg-teal-600 transition"
+        >
+          Sort by Price: {sortOrder === "asc" ? "Low to High" : "High to Low"}
+        </button>
+      </div>
 
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10 mt-10">
         {allTours.map((item) => (
